08b32bd346d85cf61de73f2526d02910ab84f2f4
Daytona1315 <155112376+Daytona1315@users.noreply.github.com>
2025-06-19 20:57:19 +0300

    Still trying to solve

--- src/db/redis.py
+++ src/db/redis.py
@@ -5,7 +5,7 @@ from src.app.utils.env_loader import Config
 
 def get_redis_connection(db: int = 0, decode_responses: bool = True):
     return redis.Redis(
-        host=Config.redis_host,
+        host='redis',
         port=Config.redis_port,
         db=db,
         decode_responses=decode_responses,
