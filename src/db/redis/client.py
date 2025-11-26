2a6a595d298e1a0d6ec3b4e04f29a85077cb19c8
Daytona1315 <osky120302@gmail.com>
2025-11-26 19:50:07 +0300

    redis host fix

--- src/db/redis/client.py
+++ src/db/redis/client.py
@@ -5,7 +5,7 @@ from src.app.utils.config import Config
 
 def get_redis_connection(db: int = 0, decode_responses: bool = True):
     return redis.Redis(
-        host="redis",
+        host=Config.redis_host,
         port=Config.redis_port,
         db=db,
         decode_responses=decode_responses,
